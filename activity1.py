ffefb2665fa57ad18ca979db47dac001ebef8708
lishanthlishanth285-lgtm <lishanthlishanth285#gmail.com>
2026-06-26 13:56:49 +0400

    functions

---
+++ activity1.py
@@ -0,0 +1,8 @@
+def wishes():
+    print("Happy morning")
+
+wishes()
+print("im back")
+
+wishes()
+print("lets close")
---
+++ activity2.py
@@ -0,0 +1,11 @@
+def wishes(times):
+    print(f"Happy {times}")
+
+wishes("morning")
+print("get to work")
+
+wishes("afternoon")
+print("take lunchbreak")
+
+wishes("evening")
+print("leave work , no overtime")
