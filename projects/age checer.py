bcb3719ee377d73ec657fcad0fdda854713f8be2
lishanthlishanth285-lgtm <lishanthlishanth285#gmail.com>
2026-07-08 23:25:23 +0400

    age checker

--- projects/age checer.py
+++ projects/age checer.py
@@ -1,2 +1,12 @@
-age = int(input("Enter your age: "))
-
+try:
+    age = int(input("Enter your age: "))
+    if age < 0:
+        raise ValueError
+    elif age == float:
+        raise ValueError
+    elif age == str:
+        raise ValueError
+    elif age > 100:
+        raise ValueError
+except ValueError:
+    print("problem: You have entered a miss matching value")
